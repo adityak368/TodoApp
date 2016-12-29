46bfb3c5fe9ea4593cc3925b703e8279d0bbb656
adityak368 <adityak368@gmail.com>
2016-12-29 14:55:16 +0530

    Initial Commit

---
+++ js/Task.js
@@ -0,0 +1,5 @@
+var Task = function(text,id) {
+	this.isComplete = false;
+	this.description = text;
+	this.id = id;
+}
---
+++ js/main.js
@@ -0,0 +1,148 @@
+var tasks = [];
+
+function initDummyTasks() {
+	for(var i=0;i<4;i++) {
+		var t = new Task(i,i);
+		if(i==5)
+			t.isComplete = true;
+		tasks.push(t);
+	}
+
+}
+
+function loadTasks(taskArr) {
+	taskArr.map(createCard);
+}
+
+function createCard(task) {
+	var card = document.createElement("div");
+	card.className = "card"; 
+	if(!task.isComplete)
+		card.appendChild(getCompleteTaskButton(task));
+	card.appendChild(getDeleteTaskButton(task));
+	card.appendChild(getTaskStatus(task));
+	card.appendChild(getTaskDesciption(task));	
+	var content = document.getElementById("content");
+	content.appendChild(card);
+}
+
+function getCompleteTaskButton (task) {
+	var completeButton = document.createElement("button");
+	completeButton.className = "complete fa fa-check-circle";
+	completeButton.id = task.id;
+	completeButton.addEventListener('click', completeTask );
+	return completeButton;
+}
+
+function getDeleteTaskButton(task) {
+	var deleteButton = document.createElement("button");
+	deleteButton.className = "delete fa fa-window-close ";
+	deleteButton.id = task.id;
+	deleteButton.addEventListener('click', deleteTask );
+	return deleteButton;
+}
+
+function getTaskDesciption(task) {
+	var textp = document.createElement("p");
+	var text = document.createTextNode(task.description);
+	textp.appendChild(text);
+	return textp;
+}
+
+function getTaskStatus(task) {
+	var status = document.createElement("div");
+	var statusText;
+	if(task.isComplete) {
+		statusText = document.createTextNode("Complete");
+		status.className = "success"; 
+	}
+	else {
+		statusText = document.createTextNode("Pending");
+		status.className = "pending"; 
+	}
+	status.appendChild(statusText);
+	return status;
+}
+
+
+function deleteTask() {
+	var taskId = this.id;
+	tasks.every(function(task, index) {
+	    if (task.id==taskId) {
+	    	tasks.splice(index,1);
+	        return false;
+	    }
+	    else return true;
+	});
+	clearContent();
+	filterTasks();
+}
+
+
+function completeTask() {
+	var taskId = this.id;
+	tasks.every(function(task, index) {
+	    if (task.id==taskId) {
+	    	tasks[index].isComplete = true;
+	        return false;
+	    }
+	    else return true;
+	});
+	clearContent();
+	filterTasks();
+}
+
+
+function filterTasks() {
+	clearContent();
+	switch(parseInt(document.getElementById("filter").value)) {
+		case 0:
+			loadTasks(tasks);
+		break;
+		case 1:
+			var result = tasks.filter(function(task) {
+					if(task.isComplete) {
+						return true;
+					}
+			});
+			loadTasks(result);
+		break;
+		case 2:
+			var result = tasks.filter(function(task) {
+					if(!task.isComplete) {
+						return true;
+					}
+			});
+			loadTasks(result);
+		break;
+		default:
+			loadTasks();
+		break;
+	}
+}
+
+
+function clearContent() {
+	var content = document.getElementById("content");
+	content.innerHTML = "";
+}
+
+function addTask() {
+	var desc = document.getElementById("taskdescription");
+	if(desc.value.trim()) {
+		var t = new Task(desc.value, tasks.length);
+		tasks.push(t);
+		hideTaskPanel();
+		filterTasks();
+		desc.value = "";
+	}
+}
+
+function openTaskPanel() {
+	document.getElementById("taskAdditionDiv").style.display = "block";
+}
+
+
+function hideTaskPanel() {
+	document.getElementById("taskAdditionDiv").style.display = "none";
+}
